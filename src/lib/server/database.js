f0a291066247a13058a2d54640c225b50964a3a5
James Brattin <james.brattin@proton.me>
2024-10-11 13:37:25 -0400

    added dotenv for local dev

--- src/lib/server/database.js
+++ src/lib/server/database.js
@@ -1,11 +1,19 @@
 import { createClient } from "@libsql/client";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const client = createClient({
-	url: process.env.DB_URL || '',
-	authToken: process.env.AUTH_TOKEN || ''
-})
+    url: process.env.DB_URL || '',
+    authToken: process.env.AUTH_TOKEN
+});
 
 export async function getAllTodos() {
-	const todos = await client.execute('SELECT * FROM todos');
-	return todos.rows;
+    try {
+        const todos = await client.execute('SELECT * FROM todos');
+        return todos.rows;
+    } catch (error) {
+        console.error('Error fetching todos:', error);
+        return [];
+    }
 }
